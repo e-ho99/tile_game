269cbddd9c21b0a6350f408deaebe10ccaae1032
e-ho99 <ethansombatt@gmail.com>
2022-03-19 21:10:18 -0500

    added toy field map obj; adjusted mapNames table to accept presentable names;

---
+++ src/server/classes/maps/toy_field.lua
@@ -0,0 +1,20 @@
+ToyField = {}
+ToyField.__index = ToyField
+--ddddd
+local timerEvents = game.ReplicatedStorage.shared.Events.TimerEvents
+
+function ToyField:init(e)
+    engine = e
+    setmetatable(ToyField, engine.classes.map)
+end
+
+function ToyField.new()
+    local self =  setmetatable(engine.classes.map.new(), ToyField)
+    self._modes = {"Disappearing Plates"}
+    self._mapTemplate = game.ServerStorage.Maps["Toy Field"]
+    self._name = "toy field"
+    
+    return self
+end
+
+return ToyField
