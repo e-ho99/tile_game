1a21c0b2a31c9cfad59e7d4f3ae7dad25017bd6e
e-ho99 <ethansombatt@gmail.com>
2022-06-18 00:38:36 -0500

    created winnersUI

--- src/client/interfaces/winnersUI.lua
+++ src/client/interfaces/winnersUI.lua
@@ -11,22 +11,38 @@ end
 function WinnersUI.new()
     local self = setmetatable(engine.interfaces.gui.new(script.Name), WinnersUI)
     
+    self._mainframe = self._gui.Winners
+    self._templates = self._gui.Templates
+
     self:initEvents()
     return self
 end
 
 function WinnersUI:initEvents()
-    local e = gameEvents.SendWinners.OnClientEvent:Connect(function(winners)
-        if winners == "" then
-            winners = "No winners"
+    local e = gameEvents.SendWinners.OnClientEvent:Connect(function(winners, ordered)
+        if #winners == 0 then
+            -- show no winners label
+            
+        else
+            -- TODO: have ordered condition + 1st, 2nd, 3rd place icons
+            for index, userid in pairs (winners) do
+                local template = self._templates.WinnerPlayer:Clone()
+                template.PlayerImage.Image = game.Players:GetUserThumbnailAsync(userid, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size100x100)
+                template.PlayerName.Text = game.Players:GetPlayerByUserId(userid).Name
+                template.Parent = self._mainframe.Container
+                template.Visible = true
+            end
         end
 
-        self._gui.Frame.Winners.Text = winners
         self._gui.Enabled = true
-
         task.wait(5)
-
         self._gui.Enabled = false
+
+        for _, playerGrid in pairs (self._mainframe.Container:GetChildren()) do
+            if playerGrid:IsA("GuiBase2d") then
+                playerGrid:Destroy()
+            end
+        end
     end)
 
     table.insert(self._events, e)
